b90de1ca5dbd1b22bd6958d72dd76942281aec62
flashmaestro <masterofflash@nate.com>
2014-03-06 00:41:19 +0900

    First Go Program

---
+++ go/leap/leap.go
@@ -0,0 +1,10 @@
+package leap
+
+func IsLeapYear(year int) bool{
+    if year % 400 == 0 {
+        return true
+    } else if year % 4 == 0 && year % 100 !=0 {
+        return true
+    }
+    return false
+}
