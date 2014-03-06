94619d0fc2e8cd993841ab25345c4ae9aacd6e77
송 종근 <masterofflash@nate.com>
2014-03-06 19:53:00 +0900

    Initial Commit

---
+++ objective-c/bob/bob/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bob
+//
+//  Created by 송 종근 on 14. 3. 6..
+//  Copyright (c) 2014년 G.B.U Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
